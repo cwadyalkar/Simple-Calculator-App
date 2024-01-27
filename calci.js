c1fe2b6164142d4c7212afb88f4cf2118b30ca27
Chirag Wadyalkar <cwadyalkar@gmail.com>
2024-01-27 10:14:52 +0530

    first commit

---
+++ calci.js
@@ -0,0 +1,25 @@
+const input = document.getElementById("inputBox");
+const buttons = document.querySelectorAll("button");
+let string = "";
+
+buttons.forEach((e) => {
+  e.addEventListener("click", (b) => {
+    if (b.target.innerText == "=") {
+      string = String(eval(string));
+      inputBox.value = string;
+    } else if (b.target.innerText == "AC") {
+      string = "";
+      inputBox.value = string;
+    } else if (b.target.innerText == "DEL") {
+      string = string.substring(0, string.length - 1);
+      inputBox.value = string;
+    } else if (b.target.id == "plusMinus") {
+      string = String(-eval(string));
+      inputBox.value = string;
+    } else {
+      string += b.target.innerText;
+      inputBox.value = string;
+    }
+  });
+});
+
